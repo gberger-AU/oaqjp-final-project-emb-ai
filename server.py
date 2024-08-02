854cc604fc65f3209181011ec4a82a0fc80f7c1f
gberger-AU <gberger@aurora.edu>
2024-08-02 12:21:13 -0400

    Completes static code analysis on server.py

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module provides a Flask web application for emotion detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +8,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """
+    Endpoint for emotion detection. Analyzes the provided text and returns the detected emotions.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -12,22 +18,25 @@ def emo_detector():
     status_code, response = emotion_detector(text_to_analyze)
 
     if status_code == 400:
-        rDict = {key: None for key in ["anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"]}
-    if status_code != 200 or response['dominant_emotion'] is 'None':
+        r_dict = {key: None for key in [
+            "anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"
+        ]}
+    if status_code != 200 or response['dominant_emotion'] == 'None':
         return "Invalid text! Please try again!"
-    else:
-        rDict = response
+    r_dict = response
 
     # Return a formatted string with the sentiment label and score
-    return ("For the given statement, the system response is 'anger': {}, "
-    "'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. "
-    "The dominant emotion is {}.").format(
-        rDict['anger'], rDict['disgust'], rDict['fear'],
-        rDict['joy'], rDict['sadness'], rDict['dominant_emotion'])
-    
+    return (
+        f"For the given statement, the system response is 'anger': {r_dict['anger']}, "
+        f"'disgust': {r_dict['disgust']}, 'fear': {r_dict['fear']}, 'joy': {r_dict['joy']} and "
+        f"'sadness': {r_dict['sadness']}. The dominant emotion is {r_dict['dominant_emotion']}."
+    )
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
